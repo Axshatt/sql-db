29c006e5242bbefa9b25bcaa695683c57a9bbbfb
Akshat <codepreneur.in@gmail.com>
2025-07-28 12:52:24 +0530

    added db

--- db.sql
+++ db.sql
@@ -29,3 +29,33 @@ CREATE TABLE addresses (
     --Foreign key 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+
+
+
+
+
+--SQL query
+-- To insert the address of a user - 
+INSERT INTO addresses (user_id, city, country, street, pincode)
+VALUES (1, 'New York', 'USA', '123 Broadway St', '10001');
+
+-- Now if you want to get the address of a user given an id , you can run the following query - 
+SELECT city, country, street, pincode
+FROM addresses
+WHERE user_id = 1;
+
+
+-- Good question to have at this point is what queries are run when the user signs up and sends both their information and their address in a single request.
+-- Do we send two SQL queries into the database? What if one of the queries (address query for example) fails? 
+-- This would require transactions  in SQL to ensure either both the user information and address goes in, or neither does
+
+BEGIN; -- Start transaction
+
+INSERT INTO users (username, email, password)
+VALUES ('john_doe', 'john_doe1@example.com', 'securepassword123');
+
+INSERT INTO addresses (user_id, city, country, street, pincode)
+VALUES (currval('users_id_seq'), 'New York', 'USA', '123 Broadway St', '10001');
+
+COMMIT;
